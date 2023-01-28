ae7c4f0b01667ca3c542a7c9c115a340a448948f
Umar <gadaevduk@gmail.com>
2023-01-28 23:26:18 +0300

    Сделал форму для отправки сообщения

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/productsSlice"
-
+import feedbackReducer from '../features/feedbackSlice'
 
 export const store = configureStore({
-    reducer: { productsReducer },
+    reducer: { productsReducer , feedbackReducer},
   });
